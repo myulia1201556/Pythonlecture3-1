7842ceff6c76c1ac470f489e061e4d6c0b3ae872
Yulia <yulia1201556@gmail.com>
2022-10-14 05:01:06 +0300

    Крестики - нолики

--- Task/003.py
+++ Task/003.py
@@ -5,76 +5,53 @@ print("Игра крестики - нолики!")
 board = list(range(1,10))
 
 
-def create_board(board):
-    print("-"*12)
+def create_board():
+    print("-"*20)
     for i in range(3):
-        print("|", board[0+i*3],"|", board[1+i*3], "|", board[2+i*3], "|")
-        print('-'*12)
+        print("   ", board[0+i*3],"   ", board[1+i*3], "   ", board[2+i*3], "    ")
+        print('-'*20)
 
 
 def choice(number):
-    valid = False
-    while not valid:
+    global board
+    while True:
         player_step = input("Ваш ход, выберите ячейку " + number + " --> ")
-        try:
+        if player_step.isdigit() and int(player_step) in range(1, 10):
             player_step = int(player_step)
-        except:
-            print("Error")
-            continue
-        if player_step >= 1 and player_step <= 9:
-            if(str(board[player_step - 1]) not in "XO"):
-                board[player_step-1] = number
-                valid = True
+            pos = board[player_step - 1]
+            if pos not in (chr(10060), chr(11093)):
+                board[player_step - 1] = chr(10060) if number == "X" else chr(11093)
+                break
             else:
                 print("Эта ячейка занята")
         else:
-            print("Попробуйте снова")
+            print(f"Попробуйте снова{chr(9940)}")
 
 
-def win_step(board):
-    win_position = [(0,1,2),(3,4,5),(6,7,8),
+
+def win_step():
+    win_position = ((0,1,2),(3,4,5),(6,7,8),
                     (0,3,6),(1,4,7),(2,5,8),
-                    (0,4,8),(2,4,6)]
-    win = ""
-    for i in win_position:
-        if board[i[0]] == "X" and board[i[1]] =="X"and board[i[2]] == "X":
-            win = "X"
-        if board[i[0]] == "O" and board[i[1]] == "O" and board[i[2]] == "O":
-            win = "O"
-    return win
+                    (0,4,8),(2,4,6))
+    n = [board[x[0]] for x in win_position if board[x[0]] == board[x[1]] == board[x[2]]]
+    return n[0] if n else n
 
-    for i in win_position:
-        if board[i[0]] == board[i[1]] == board[i[2]]:
-            return board[i[0]]
-    return False
-
-
-
-def game(board):
+    
+def game():
     counter = 0
-    winner = False
-    while not winner:
-        create_board(board)
-        if counter % 2 == 0:
-            choice("X")
-        else:
-            choice("0")
-        counter +=1
-        if counter > 4:
-            tmp = win_step(board)
-            if tmp:
-                print(tmp,"Победа")
-                winner = True
+    create_board()
+    while True:
+        choice("O") if counter % 2 else choice("X")
+        create_board()
+
+        if counter > 3:
+            if win_step():
+                print(f"{win_step()} - Выиграл{chr(127942)}{chr(127881)}!")
                 break
-            if counter == 9:
-                print("Ничья!)")
-        create_board(board)
-       
-
-game(board)
+        if counter == 8:
+            print(f"Ничья {chr(129318)}{chr(129309)}!")
+            break
+        counter += 1
 
 
-
-
-
-
+game()
